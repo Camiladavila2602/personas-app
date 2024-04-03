622dcaf5740b96d712a39a7ec931e3e223633a58
Camila Davila <camiladavila151015@gmail.com>
2024-04-02 23:17:50 -0500

    add

--- app/Http/Controllers/ComunaController.php
+++ app/Http/Controllers/ComunaController.php
@@ -16,7 +16,64 @@ class ComunaController extends Controller
     public function index()
     {
         $comunas = Comuna::all();
+        $comunas = DB::table('tb_comuna')
+        ->join('tb_municipio', 'tb_comuna.muni_codi', '=','tb_comuna.muni_codi')
+        ->select ('tb_comuna.*',"tb_municipio.muni_nomb")
+        ->get();
+        
         return view('comuna.index',['comunas'=> $comunas]);
 
     }
+/**
+     * Show the form for creating a new resource.
+     * 
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $municipios =DB::table('tb_municipio')
+        ->orderBy('muni_nomb')
+        ->get();
+        return view('comuna.new',['municipios'=> $municipios]);
+
+    }
+ /**
+     * Store a newly created resource in storage.
+     * 
+     * @param \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $comuna = new Comuna();
+        //$comuna->comu_codi = $request->id;
+        $comuna->comu_nomb=$request->name;
+        $comuna->muni_codi=$request->code;
+        $comuna->save();
+
+        $comunas = DB::table('tb_comuna')
+        ->join('tb_municipio','tb_comuna.muni_codi', '=','tb_municipio.muni_codi')
+        ->select('tb_comuna.*',"tb_municipio.muni_nomb")
+        ->get();
+        return view('comuna.index',['comunas'=> $comunas]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     * 
+     * @param int $sid 
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $comuna = Comuna::find($id);
+       $comuna->delete();
+
+        $comunas = DB::table('tb_comuna')
+        ->join('tb_municipio','tb_comuna.muni_codi', '=','tb_municipio.muni_codi')
+        ->select('tb_comuna.*',"tb_municipio.muni_nomb")
+        ->get();
+        return view('comuna.index',['comunas'=> $comunas]);
+
+}
 }
--- resources/views/comuna/index.blade.php
+++ resources/views/comuna/index.blade.php
@@ -15,7 +15,8 @@
 
   <body>
     <div class="container">
-    <h1>Commune List</h1>
+    <h1>Listado de comunas</h1>
+    <a href="{{ route('comunas.create')}}" class="btn btn-success"> Add</a>
     <table class="table">
   <thead>
     <tr>
@@ -30,8 +31,15 @@
     <tr>
       <th scope="row">{{$comuna->comu_codi}}</th>
       <td>{{ $comuna->comu_nomb}}</td>
-      <td>{{ $comuna->muni_codi}}</td>
-      <td><span> Actions </span></td>
+      <td>{{ $comuna->muni_nomb}}</td>
+      <td>
+        <form action="{{ route('comunas.destroy',['comuna'-> $comuna->comu_codi]) }}"
+        method ='POST' style="display: inline-block">
+        @method('delete')
+        @csrf
+        <input class="btn btn-danger" type="submit" value="Delete">
+</form>
+      </td>
     </tr>
     @endforeach
   </tbody>
